9012772118af1974f7734c07e2aacab41ed7014a
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-10-17 23:48:56 +0500

    Task1

---
+++ Tyuiu.AlshinAF.Sprint2.Task1.V8.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AlshinAF.Sprint2.Task1.V8.Lib
+{
+    public class DataService : ISprint2Task1V8
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a + 1 == b)|(c + 1 < a);
+            res[1] = (c + 1 != a)&(a>d);
+            res[2] = (a>c)||(b!=d);
+            res[3] = (c>=d)&&(a>d);
+            res[4] = ((d>=b) == !(a!=c));
+            res[5] = (a - 1 == c) ^ (b <= d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AlshinAF.Sprint2.Task1.V8/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.AlshinAF.Sprint2.Task1.V8.Lib;
+namespace Tyuiu.AlshinAF.Sprint2.Task1.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 15;
+            int b = 16;
+            int c = 14;
+            int d = 335;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #2 | Выполнил: Альшин А. Ф. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:  Логические операции                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Альшин Артур Фаритович | РППб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать,но использовать один раз в выражении)*");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^,последовательность операций   *");
+            Console.WriteLine("* не должна нарушаться), а также арифметических выражений, которая вернет *");
+            Console.WriteLine("* массив: (True,False,True,False,True,False), при a=15,b=16,c=14,d=335    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
