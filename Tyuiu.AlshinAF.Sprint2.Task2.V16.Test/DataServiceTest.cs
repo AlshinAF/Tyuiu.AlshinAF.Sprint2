f695e8abe4a3fc614becb2a1f25144dc43cf3088
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-10-18 21:59:26 +0500

    Task2

---
+++ Tyuiu.AlshinAF.Sprint2.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using System.Net.Http.Headers;
+using Tyuiu.AlshinAF.Sprint2.Task2.V16.Lib;
+namespace Tyuiu.AlshinAF.Sprint2.Task2.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
