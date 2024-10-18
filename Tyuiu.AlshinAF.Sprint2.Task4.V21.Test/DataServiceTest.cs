d2129b33e208dbf95d75a956dc8dd5707d4d0612
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-10-19 00:44:15 +0500

    Task4

---
+++ Tyuiu.AlshinAF.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+using Tyuiu.AlshinAF.Sprint2.Task4.V21.Lib;
+namespace Tyuiu.AlshinAF.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 4;
+            double wait = 0.405;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = 20.797;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlshinAF.Sprint2.Task4.V21/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.AlshinAF.Sprint2.Task4.V21.Lib;
+double x = 2;
+double y = 3;
+DataService ds = new DataService();
+Console.WriteLine(ds.Calculate(x, y));
