a3b1b6ce1f0832dc14e3b2313d8d9957f651d194
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-10-19 03:58:19 +0500

    Task7

--- Tyuiu.AlshinAF.Sprint2.Task7.V12.Test/DataServiceTest.cs
+++ Tyuiu.AlshinAF.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.AlshinAF.Sprint2.Task7.V12.Test
             double x = 0.1;
             double y = 0;
             bool res = ds.CheckDotInShadedArea(x, y);
-            bool wait = false ;
+            bool wait = true ;
             Assert.AreEqual(wait, res);
         }
     }
